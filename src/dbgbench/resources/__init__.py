65a52fb6dd738e16d37eea32444c616e199c84ba
Martin Eberlein <schuldner-01.vordach@icloud.com>
2025-01-30 21:40:21 +0100

    added get_samples function

--- src/dbgbench/resources/__init__.py
+++ src/dbgbench/resources/__init__.py
@@ -1,12 +1,16 @@
 import importlib.resources as pkg_resources
 
 
-def get_grep_grammar():
+def get_grep_grammar_path():
     return pkg_resources.files("dbgbench.resources.fandango") / "grep.fan"
 
 
 def get_grep_samples_dir():
     return pkg_resources.files("dbgbench.resources.samples") / "grep"
+
+def get_grep_samples():
+    sample_dir = get_grep_samples_dir()
+    return [file.read_text() for file in sample_dir.iterdir() if file.is_file()]
 
 
 def get_islearn_pattern_file_path():
