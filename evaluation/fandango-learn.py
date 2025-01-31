bbb9ed6d1d61612c9a6dad552b2e2a87ebe9cfc0
Martin Eberlein <33397273+martineberlein@users.noreply.github.com>
2025-01-31 17:48:50 +0100

    started using the new mutation fuzzer to add additional inputs

--- evaluation/fandango-learn.py
+++ evaluation/fandango-learn.py
@@ -1,22 +1,47 @@
+from os.path import samestat
+
 from dbgbench.framework.oraclesresult import OracleResult
 from dbgbench.resources import get_grep_grammar_path, get_grep_samples
 from dbgbench.framework.util import escape_non_ascii_utf8
-from dbgbench.subjects import Grep3c3bdace
+from dbgbench.subjects import Grep3c3bdace, Grepc96b0f2c
 
 from fandangoLearner.interface.fandango import parse
 from fandangoLearner.data.input import FandangoInput
 from fandangoLearner.learner import FandangoLearner
 from fandangoLearner.logger import LoggerLevel
 from fandangoLearner.resources.patterns import Pattern
+from fandangoLearner.refinement.mutation import MutationFuzzer
+
+
+def generate_more_failing(bug_type_, grammar_, samples_):
+
+    def bug_oracle(inp):
+        with bug_type_() as bug:
+            res = bug.execute_sample(str(inp))
+        return res
+
+    samples_ = [
+        FandangoInput.from_str(grammar_, escape_non_ascii_utf8(inp), bug_oracle(inp)) for inp in samples_
+    ]
+
+    seeds = [inp for inp in samples_ if inp.oracle == OracleResult.FAILING]
+
+    mutation_fuzzer = MutationFuzzer(grammar_, seed_inputs=seeds, oracle=bug_oracle)
+
+    for inp in list(mutation_fuzzer.run()):
+        print(inp)
 
 
 if __name__ == "__main__":
+    bug_type = Grepc96b0f2c
+
     grep_grammar = get_grep_grammar_path()
     grammar, _ = parse(grep_grammar)
 
-    #bug = Grep3c3bdace()
+    generate_more_failing(bug_type, grammar, get_grep_samples())
+
     samples = get_grep_samples()
-    with Grep3c3bdace() as bug:
+    with bug_type() as bug:
         result = bug.execute_samples(samples)
 
     test_inputs = []
@@ -32,18 +57,18 @@ if __name__ == "__main__":
     for _ in range(100):
         additional_inputs.append(str(grammar.fuzz(max_nodes=100)))
 
-    # with Grep3c3bdace() as bug:
-    #     result = bug.execute_samples(additional_inputs)
-    #
-    # test_inputs = []
-    # for inp, oracle in result:
-    #     oracle_bool = True if oracle == OracleResult.BUG else False
-    #     test_inputs.append((inp, oracle_bool))
-    #
-    # add_initial_inputs = {
-    #     FandangoInput.from_str(grammar, inp, oracle) for inp, oracle in test_inputs
-    # }
-    # initial_inputs.update(add_initial_inputs)
+    with bug_type() as bug:
+        result = bug.execute_samples(additional_inputs)
+
+    test_inputs = []
+    for inp, oracle in result:
+        oracle_bool = True if oracle == OracleResult.BUG else False
+        test_inputs.append((inp, oracle_bool))
+
+    add_initial_inputs = {
+        FandangoInput.from_str(grammar, inp, oracle) for inp, oracle in test_inputs
+    }
+    initial_inputs.update(add_initial_inputs)
 
 
     patterns = [
@@ -55,7 +80,7 @@ if __name__ == "__main__":
         # )
     ]
 
-    learner = FandangoLearner(grammar,patterns=patterns, logger_level=LoggerLevel.INFO)
+    learner = FandangoLearner(grammar, patterns=patterns, logger_level=LoggerLevel.INFO)
 
     learned_constraints = learner.learn_constraints(
         initial_inputs
