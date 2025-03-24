470bdad4b97505d764c1b163898a30038d0a8c62
Martin Eberlein <33397273+martineberlein@users.noreply.github.com>
2025-03-24 15:05:44 +0100

    refactor: streamline data loading and input generation for Grep bug types

--- evaluation/fandango-learn.py
+++ evaluation/fandango-learn.py
@@ -4,52 +4,37 @@ from typing import Collection
 from dbgbench.framework.oraclesresult import OracleResult
 from dbgbench.resources import get_grep_grammar_path, get_grep_samples
 from dbgbench.framework.util import escape_non_ascii_utf8
-from dbgbench.subjects import Grep3c3bdace, Grepc96b0f2c
+from dbgbench.framework.data import load, load_from_files
 
 from fdlearn.interface.fandango import parse
 from fdlearn.data.input import FandangoInput
 from fdlearn.learner import FandangoLearner
 from fdlearn.logger import LoggerLevel
 from fdlearn.resources.patterns import Pattern
-from fdlearn.refinement.mutation import MutationFuzzer
+from fdlearn.learning.metric import RecallPriorityFitness, F1ScoreFitness
 
-
-def generate_more_failing(bug_type_, grammar_, samples_: Collection[FandangoInput]) -> tuple[list[FandangoInput], list[FandangoInput]]:
-
-    def bug_oracle(inp):
-        with bug_type_() as bug:
-            res = bug.execute_sample(str(inp.tree))
-            print(inp, res)
-        return res
-
-    seeds = [inp for inp in samples_ if inp.oracle.is_failing()]
-
-    mutation_fuzzer = MutationFuzzer(grammar_, seed_inputs=seeds, oracle=bug_oracle)
-
-    positive_inputs = []
-    negative_inputs = []
-
-    while len(positive_inputs) < 5:
-        try:
-            inp = next(mutation_fuzzer.run(yield_negatives=True))
-            if inp.oracle == OracleResult.FAILING:
-                positive_inputs.append(inp)
-            else:
-                negative_inputs.append(inp)
-        except StopIteration:
-            break
-
-    return positive_inputs, negative_inputs
 
 if __name__ == "__main__":
     random.seed(1)
-    bug_type = Grep3c3bdace
+
+    from dbgbench.subjects import Grep3c3bdace, Grepc96b0f2c, Grep7aa698d3, Grep3220317a
+    bug_type = Grep3220317a
+
 
     grep_grammar = get_grep_grammar_path()
     grammar, _ = parse(grep_grammar)
 
-    samples = get_grep_samples()
+    evaluation_data = []
+    positive_data = load_from_files(f"../scripts/data_generation/{bug_type.__name__}/positive_inputs")
+    positive_inputs = {FandangoInput.from_str(grammar, inp, True) for inp in positive_data}
+
+    negative_data = load_from_files(f"../scripts/data_generation/Grep7aa698d3/negative_inputs")[:100]
+    negative_inputs = {FandangoInput.from_str(grammar, inp, False) for inp in negative_data}
+
+    #samples = get_grep_samples()
     with bug_type() as bug:
+        samples_paths = bug.sample_files(get_all=True)
+        samples = [file.read_text() for file in samples_paths]
         result = bug.execute_samples(samples)
 
     test_inputs = []
@@ -63,12 +48,6 @@ if __name__ == "__main__":
     for inp in initial_inputs:
         print(inp, inp.oracle)
 
-    pos_inputs, neg_inputs = generate_more_failing(bug_type, grammar, initial_inputs)
-    print(f"Positive inputs: {len(pos_inputs)}")
-    print(f"Negative inputs: {len(neg_inputs)}")
-    initial_inputs.update(pos_inputs)
-    initial_inputs.update(neg_inputs)
-
     patterns = [
         Pattern(
             string_pattern="exists <elem> in <NON_TERMINAL>: is_inside(<elem>, <start>);",
@@ -78,11 +57,22 @@ if __name__ == "__main__":
         )
     ]
 
-    learner = FandangoLearner(grammar, patterns=patterns, logger_level=LoggerLevel.INFO)
+    # initial_inputs = list(positive_inputs) + list(negative_inputs)
+
+    learner = FandangoLearner(grammar, patterns=patterns, logger_level=LoggerLevel.INFO) # +, max_conjunction_size=3)
 
     learned_constraints = learner.learn_constraints(
         initial_inputs
     )
+    #
+    # for constraint in learned_constraints:
+    #     print(constraint)
 
+    for constraint in learned_constraints:
+        constraint.evaluate(positive_inputs)
+        constraint.evaluate(negative_inputs)
+
+    sorting_strategy = RecallPriorityFitness()
+    learned_constraints = sorted(learned_constraints, key=lambda exp: sorting_strategy.evaluate(exp), reverse=True)
     for constraint in learned_constraints:
         print(constraint)
